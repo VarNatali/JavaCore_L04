1bfaed880bf49c40363247e55240267d329fa9d1
VarNatali <nataliiavaren@gmail.com>
2023-11-22 08:36:20 +0100

    Форматовано

--- src/ua/lviv/lgs/Robot/Application.java
+++ src/ua/lviv/lgs/Robot/Application.java
@@ -2,20 +2,22 @@ package ua.lviv.lgs.Robot;
 
 public class Application {
     public static void main(String[] args) {
-        Robot r=new Robot();
-        RobotCoocker rc=new RobotCoocker();
-        RobotDancer rd=new RobotDancer();
-        CoffeRobot cr=new CoffeRobot();
+        Robot r = new Robot();
+        RobotCoocker rc = new RobotCoocker();
+        RobotDancer rd = new RobotDancer();
+        CoffeRobot cr = new CoffeRobot();
 
 
-         r.work();      cr.work();
-         rd.work();     rc.work();
+        r.work();
+        cr.work();
+        rd.work();
+        rc.work();
 
-        System.out.println ( "" );
-        Robot rwork[]= {r,rc,rd,cr}; //масив класу Robot , заповнити масив екземплярами вищеописаних класів
-        for(int i=0; i< rwork.length; i++){
-           rwork[i].work();
-       }
+        System.out.println("");
+        Robot rwork[] = {r, rc, rd, cr}; //масив класу Robot , заповнити масив екземплярами вищеописаних класів
+        for (int i = 0; i < rwork.length; i++) {
+            rwork[i].work();
+        }
 
     }
 }
